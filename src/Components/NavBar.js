69e04d28cafb5e142b15441be31dfcb9b277d619
hadithya <hhadithya34@gmail.com>
2024-04-29 12:27:44 +0530

    UPDATE navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,20 +6,23 @@ import { FiShoppingBag } from "react-icons/fi";
 function ColorSchemesExample() {
   return (
     <>
-      <Navbar bg="light" data-bs-theme="light" fixed='top'>
+     <Navbar bg="light" expand="lg" fixed="top">
         <Container>
           <Navbar.Brand href="#home">FashionFirst</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Men</Nav.Link>
-            <Nav.Link href="#features">Women</Nav.Link>
-            <Nav.Link href="#pricing">E-Voucher</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#cart">
-                <FiShoppingBag />
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Men</Nav.Link>
+              <Nav.Link href="#features">Women</Nav.Link>
+              <Nav.Link href="#pricing">E-Voucher</Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link href="#login">Login</Nav.Link>
+              <Nav.Link href="#cart">
+                  <FiShoppingBag />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
