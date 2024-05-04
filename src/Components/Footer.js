a72342cfcaa18a86e28980ee06c7199c64a73e01
hadithya <hhadithya34@gmail.com>
2024-05-04 21:07:38 +0530

    footer done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,52 +1,50 @@
-import React from 'react';
-import '../Stylesheets/footer.css';
+import '../Stylesheets/Footer.css';
+import { FaFacebook, FaInstagram, FaYoutube, FaTiktok } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJChzreeLkSgqlE_XVfx_IeQotu7HK7P1mM3mlHyAZFw&s" alt="Company Logo" />
-                            <p>FOLLOW US</p>
-                            <div class="social-icons">
-                                <i class="fab fa-facebook facebook-icon"></i>
-                                <i class="fab fa-instagram"></i>
-                                <i class="fab fa-youtube"></i>
-                                <i class="fab fa-tiktok"></i>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src="/Images/logo-white.png" alt="Company Logo" />
+                        <div className="d-flex flex-column gap-1">
+                            <p style={{ marginBottom: '1%' }} className='heading'>FOLLOW US</p>
+                            <div className="d-flex gap-4">
+                                <FaInstagram size={25} />
+                                <FaFacebook size={25} />
+                                <FaTiktok size={25}  />
+                                <FaYoutube size={25}  />
                             </div>
-                            <p style={{paddingTop: '60px'}}>&copy; 2024 FOA Clothing. All rights reserved. Powered by Pentacove</p>
                         </div>
-                    <div class="col-md-6">
-                        <p>SIGN UP FOR THE NEWSLETTER</p>
-                        <p>Be the first to know about our new collection and promotions.</p>
-                        <form class="row g-2 align-items-center">
-                                <div class="col-auto">
-                                    <label for="emailInput" class="visually-hidden">Email address</label>
-                                    <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" />
-                                </div>
-                                <div class="col-auto">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                </div>
-                                <div id="emailHelp" class="form-text col-12">We'll never share your email with anyone else.</div>
-                            </form>
-                        <div class="row" style={{marginTop: '10px'}}>
-                            <div class="col-md-6">
-                                <p>SUPPORT</p>
-                                <p class="text1">Terms and conditions</p>
-                                <p class="text1">Privacy Policy</p>
-                                <p class="text1">Refund Policy</p>
-                                <p class="text1">Shipping Policy</p>
-                            </div>
-                            <div class="col-md-6">
-                                <p>INFO</p>
-                                <p class="text1">Our Story</p>
-                                <p class="text1">Contact Us</p>
-                                <p class="text1">Careers at FOA</p>
-                                <p class="text1">Training Dept</p>
-                            </div>
-                        </div>   
                     </div>
+                    <div className="col-md-6">
+                        <p className='heading'>SIGN UP FOR THE NEWSLETTER</p>
+                        <p style={{ fontWeight: 'lighter'}}>Be the first to know about our new collection and promotions.</p>
+                        <form className="row g-2 align-items-center">
+                            <div className="col-auto">
+                                <label htmlFor="emailInput" className="visually-hidden">Email address</label>
+                                <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Email" />
+                            </div>
+                        </form>
+                        <div className="row">
+                            <div className="col-md-5">
+                                <p className='heading'>SUPPORT</p>
+                                <p className="text">Terms and conditions</p>
+                                <p className="text">Privacy Policy</p>
+                                <p className="text">Refund Policy</p>
+                                <p className="text">Shipping Policy</p>
+                            </div>
+                            <div className="col-md-5">
+                                <p className='heading'>INFO</p>
+                                <p className="text">Our Story</p>
+                                <p className="text">Contact Us</p>
+                                <p className="text">Careers at FOA</p>
+                                <p className="text">Training Dept</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p style={{color: 'rgb(175,175,175)', fontWeight: 'lighter', paddingTop:'2%'}}>&copy; 2024 KONIK Clothing. All rights reserved. Powered by FIT</p>
                 </div>
             </div>
         </footer>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './Pages/Home';
-import ColorSchemesExample from './Components/NavBar';
-import Hero from './Components/Hero';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import SignIn from './Pages/SignIn';
-// import ProductView from './Components/ProductView';
 import Footer from './Components/Footer';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <SignIn /> */}
-    <Hero />
-    <ColorSchemesExample />
-    {/* <DarkVariantExample /> */}
-    <Home />
-    {/* <ProductView /> */}
     <Footer />
   </React.StrictMode>
 );
