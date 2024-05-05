8d00a0eb7e7ad2bc9f6cb4b2a665513c9fa718c0
hadithya <hhadithya34@gmail.com>
2024-05-05 17:51:58 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import ProductView from './Pages/ProductView';
 import CheckOutCard from './Components/CheckOutCard';
-import Collection from './Pages/Collection';
+import MensCollection from './Pages/MensCollection';
 import LogIn from './Pages/LogIn';
 import SignUp from './Pages/SignUp';
 
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product-view/:id" element={<ProductView />} />
         <Route path="/checkout" element={<CheckOutCard />} />
-        <Route path="/collection" element={<Collection />} />
+        <Route path="/menscollection" element={<MensCollection />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
---
+++ src/Pages/MensCollection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+import '../Stylesheets/Collection.css';
+
+function MensCollection(){
+    return (
+        <>
+            <Navbar />
+            <div className='image-container'>
+                <img src="/Images/tops.webp" alt="konik t shirt" className='back'/>
+                <p>Men Casual</p>
+            </div>
+            <Footer />
+        </>
+      );
+}
+export default MensCollection;
+
