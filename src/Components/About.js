d6e2a5a360be0c33ab3402a63462b6849fa1b81a
hadithya <hhadithya34@gmail.com>
2024-05-05 19:18:12 +0530

    about us updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 function About() {
     const contentStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
         padding: '120px',
+        backgroundColor: '#f6f6f6',
+        marginTop: '2.5%',
     };
       
     const textStyle = {
@@ -14,8 +13,8 @@ function About() {
     };
     
     return (
-        <div style={contentStyle} className="content container w-100">
-          <h1 className="text-center" style={{letterSpacing:'1.1rem', fontWeight: '600', padding: '5% 0 1% 0', color: '#151515', fontSize: '2.5rem'}}>WELCOME TO KONIK</h1>
+        <div style={contentStyle} className="content">
+          <h1 className="text-center" style={{letterSpacing:'1.1rem', fontWeight: '600', padding: '5% 0 1% 0', color: '#151515', fontSize: '2.5rem'}}>DISCOVER THE BEST</h1>
           <h2 style={textStyle}>Your Destination for Premium Men's T-Shirts in Sri Lanka!</h2>
           <p style={textStyle}>Elevate your style with our meticulously curated collection, blending comfort and sophistication seamlessly. From casual classics to refined essentials, each garment is crafted with precision and passion, ensuring unparalleled quality and style. Explore our range today and redefine your wardrobe with Konik's signature blend of luxury and versatility.</p>
         </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import '../Stylesheets/footer.css';
+import '../Stylesheets/Footer.css';
 import { FaFacebook, FaInstagram, FaYoutube, FaTiktok } from 'react-icons/fa';
 
 const Footer = () => {
