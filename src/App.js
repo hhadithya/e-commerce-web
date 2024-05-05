d92d4383fd411cac3f03f926685c6cd785c0336a
hadithya <hhadithya34@gmail.com>
2024-05-05 17:56:17 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import ProductView from './Pages/ProductView';
 import CheckOutCard from './Components/CheckOutCard';
-import MensCollection from './Pages/MensCollection';
+import Collection from './Pages/Collection';
 import LogIn from './Pages/LogIn';
 import SignUp from './Pages/SignUp';
 
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product-view/:id" element={<ProductView />} />
         <Route path="/checkout" element={<CheckOutCard />} />
-        <Route path="/menscollection" element={<MensCollection />} />
+        <Route path="/collection" element={<Collection />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
