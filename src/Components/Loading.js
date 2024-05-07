e3f9d2202d36bc2d57a9350ad0bb10433890450d
hadithya <hhadithya34@gmail.com>
2024-05-07 23:29:40 +0530

    loader updated

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -15,14 +15,14 @@ function Loading() {
         }, 3000);
     }, [navigate]);
 
-
     return (
         <>
             <div className='loaderBack'>
                 {
                     showImg? (
-                        <div className="loading">
+                        <div className="loading d-flex">
                             <img src="/Images/loading.gif" alt="Loading" />
+                            <h2 style={{letterSpacing:'1.1rem', fontWeight: '600', color: '#151515', padding: '5.5% 0 0 2%',}}>KONIK CLOTHINGS</h2>
                         </div>
                     ) : (
                         <h1>{text}</h1>
