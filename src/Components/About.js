e612a3c684b7a3e841d29d95fc551df7b0a4a76f
hadithya <hhadithya34@gmail.com>
2024-05-08 13:09:04 +0530

    minor change

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,12 +13,10 @@ function About() {
     };
     
     return (
-        <div style={{...contentStyle, display: 'flex', justifyContent: 'center', alignItems: 'center'}} className="content">
-            <div style={{maxWidth: '800px'}}>
-                <h1 className="discover" style={{letterSpacing:'1.1rem', fontWeight: '600', padding: '5% 0 1% 0', color: '#151515', fontSize: '2.5rem'}}>DISCOVER THE BEST</h1>
-                <h2 style={textStyle}>Your Destination for Premium Men's T-Shirts in Sri Lanka!</h2>
-                <p style={textStyle}>Elevate your style with our meticulously curated collection, blending comfort and sophistication seamlessly. From casual classics to refined essentials, each garment is crafted with precision and passion, ensuring unparalleled quality and style. Explore our range today and redefine your wardrobe with Konik's signature blend of luxury and versatility.</p>
-            </div>
+        <div style={contentStyle} className="content">
+          <h1 className="text-center" style={{letterSpacing:'1.1rem', fontWeight: '600', padding: '5% 0 1% 0', color: '#151515', fontSize: '2.5rem'}}>DISCOVER THE BEST</h1>
+          <h2 style={textStyle}>Your Destination for Premium Men's T-Shirts in Sri Lanka!</h2>
+          <p style={textStyle}>Elevate your style with our meticulously curated collection, blending comfort and sophistication seamlessly. From casual classics to refined essentials, each garment is crafted with precision and passion, ensuring unparalleled quality and style. Explore our range today and redefine your wardrobe with Konik's signature blend of luxury and versatility.</p>
         </div>
     );
 }
