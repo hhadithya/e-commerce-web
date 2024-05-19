0448dfa8d000614d95ec5cf43475ca3ce0877255
hadithya <hhadithya34@gmail.com>
2024-05-20 04:34:46 +0530

    api error netlify - couldn't resolve

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_API_KEY",
+  apiKey: "AIzaSyB8BPvqY0EdurQBfx_BySSxKIYV_Q1vebU",
   authDomain: "e-commerce-web-5fus.firebaseapp.com",
   projectId: "e-commerce-web-5fus",
   storageBucket: "e-commerce-web-5fus.appspot.com",
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: "1:953680942798:web:969782825fb4cc35c4e29f"
 };
 
-console.log(process.env.REACT_APP_API_KEY);
+// console.log(process.env.REACT_APP_API_KEY);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
