523ba99d5c9589d66c2aac455d8dc4f77413bd6d
hadithya <hhadithya34@gmail.com>
2024-05-24 02:39:31 +0530

    minor changes

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -92,7 +92,7 @@ const Cart = () => {
                                 </td>
                                 <td>
                                     <h5 className="mt-4" style={{fontSize: '1rem'}}>
-                                        {item.total.toFixed(2)}
+                                        ${item.total.toFixed(2)}
                                     </h5>
                                 </td>
                             </tr>
